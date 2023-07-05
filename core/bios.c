3a3e735451996a0e016dd0fdf0bebff726285e3b
NCommander <ncommander@restless.systems>
2023-07-05 09:36:10 -0400

    Check EAX and not ECX when using fallback e88h method
    Signed-off-by: NCommander <ncommander@restless.systems>

--- core/bios.c
+++ core/bios.c
@@ -627,7 +627,7 @@ static int bios_scan_memory(scan_memory_callback_t callback, void *data)
     ireg.eax.w[0] = 0x8800;
     __intcall(0x15, &ireg, &oreg);
     if (!(oreg.eflags.l & EFLAGS_CF) && oreg.eax.w[0]) {
-	rv = callback(data, (addr_t) 1 << 20, oreg.ecx.w[0] << 10, SMT_FREE);
+	rv = callback(data, (addr_t) 1 << 20, oreg.eax.w[0] << 10, SMT_FREE);
 	if (rv)
 	    return rv;
     }
